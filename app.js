22c9d7a8fa28ad50725badb9839649ad850093c2
Armen Vartan <vartan.armen@gmail.com>
2015-04-08 17:16:26 -0400

    added node-uuid

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var users = require('./routes/users');
 
 var app = express();
 
+var io = require('socket.io')
+var sio = io.listen(app)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
