a913d7b62c0cb42e461f868a4e7dacb2c2bd814d
Armen Vartan <vartan.armen@gmail.com>
2015-04-10 16:34:41 -0400

    sockets

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -18,62 +18,45 @@ var asshole = {
   y: 0
 }
 
-
-var monster = {
-  image: new Image(),
-  x: 0,
-  y: 0
-}
-
-var monstersCaught = 0
-
 function reset() {
-  hero.x = canvas.width / 2
-  hero.y = canvas.height / 2
+  hero.x = canvas.width / 3
+  hero.y = canvas.height / 3
+  asshole.x = canvas.width / 1.5
+  asshole.y = canvas.height / 1.5
 
-  monster.x = 32 + (Math.random() * (canvas.width - 96))
-	monster.y = 32 + (Math.random() * (canvas.height - 96))
 }
 
-function update(modifier, player) {
-  if(37 in keysDown && player.x > 32) { player.x -= player.speed * modifier }
-  if(38 in keysDown && player.y > 32) { player.y -= player.speed * modifier }
-  if(39 in keysDown && player.x < canvas.width - 64) { player.x += player.speed * modifier }
-  if(40 in keysDown && player.y < canvas.height - 64) { player.y += player.speed * modifier }
-
-  if(
-    player.x <= (monster.x + 32) && monster.x <= (player.x + 32) &&
-    player.y <= (monster.y + 32) && monster.y <= (player.y + 32)
-  ) {
-    ++monstersCaught
-    reset()
-  }
+function update(modifier) {
+  (function(player){
+    if(37 in keysDown && player.x > 32) { player.x -= player.speed * modifier }
+    if(38 in keysDown && player.y > 32) { player.y -= player.speed * modifier }
+    if(39 in keysDown && player.x < canvas.width - 64) { player.x += player.speed * modifier }
+    if(40 in keysDown && player.y < canvas.height - 64) { player.y += player.speed * modifier }
+  })(player)
 }
 
 function broadcastPosition(player) {
-  setInterval(function() { socket.emit('playerPosition', {"x": player.x, "y": player.y}) }, 120)
+  setInterval(function() { socket.emit('playerPosition', {"x": player.x, "y": player.y}) }, 15)
 }
 
-function syncPosition(hero) {
+function syncPosition(otherPlayer) {
   setInterval(function() {
     socket.on('playerPosition', function(player) {
-      hero.x = player.x
-      hero.y = player.y
+      otherPlayer.x = player.x
+      otherPlayer.y = player.y
     })
-  }, 300)
+  }, 15)
 }
 
 function render() {
   ctx.drawImage(bgImage, 0, 0)
   ctx.drawImage(hero.image, hero.x, hero.y)
   ctx.drawImage(asshole.image, asshole.x, asshole.y)
-  ctx.drawImage(monster.image, monster.x, monster.y)
 
   ctx.fillStyle = "rgb(250, 250, 250)"
   ctx.font = "24px Inconsolata"
   ctx.textAlign = "left"
   ctx.textBaseline = "top"
-  ctx.fillText("Monsters caught: " + monstersCaught, 32, 32)
 }
 
 function main(player) {
@@ -81,7 +64,7 @@ function main(player) {
   var now = Date.now();
   if(then) {
     var delta = now - then;
-    update(delta / 1000, player);
+    update(delta / 1000);
     render();
   }
   then = now;
@@ -94,7 +77,6 @@ $(document).ready(function() {
   bgImage.src = "/images/background.png"
   hero.image.src = "/images/hero.png"
   asshole.image.src = "/images/hero.png"
-  monster.image.src = "/images/monster.png"
 
   canvas = $('#canvas')[0]
   ctx = canvas.getContext("2d")
